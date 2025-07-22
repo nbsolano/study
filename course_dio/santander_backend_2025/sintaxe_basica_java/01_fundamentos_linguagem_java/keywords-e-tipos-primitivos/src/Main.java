cade1e994d5e06789e252e6e2db3acb03f25683c
nbsolano <nathanbelmonte.ns@gmail.com>
2025-07-22 10:33:06 -0300

    chore: atualiza .gitignore para manter apenas src/

---
+++ course_dio/santander_backend_2025/sintaxe_basica_java/01_fundamentos_linguagem_java/keywords-e-tipos-primitivos/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte number = 1;
+        short number1 = 1;
+        long number2 = 1L;
+        int number3 = 1;
+        float number4 = 1.0f;
+        double number5 = 1.0d;
+        char character = 'a';
+        boolean numberTrue = true;
+    }
+}
