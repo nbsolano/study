17cd466f46ac83b79494dc26f89daf82ae5f41c5
Nathan Solano <nathanbelmonte.ns@gmail.com>
2025-08-12 13:48:32 -0300

    feat: Adicionando aula sobre herança e polimorfismo

---
+++ course_dio/santander_backend_2025/programação_orientada_a_objetos/02_Herança_e_polimorfismo/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printEmployee(new Manager());
+        printEmployee(new Salesman());
+    }
+
+    public static void printEmployee(Employee employee) {
+
+        System.out.printf("======%s======\n", employee.getClass().getCanonicalName());
+
+        switch (employee) {
+            case Manager manager -> {
+                manager.setCode("123");
+                manager.setName("João");
+                manager.setSalary(5000);
+                manager.setLogin("joao");
+                manager.setPassword("123456");
+                manager.setCommission(1200);
+
+                System.out.println(manager.getCode());
+                System.out.println(manager.getName());
+                System.out.println(manager.getSalary());
+                System.out.println(manager.getLogin());
+                System.out.println(manager.getPassword());
+                System.out.println(manager.getCommission());
+                System.out.println(manager.getfullSalary(500));
+            }
+            case Salesman salesman -> {
+                salesman.setCode("456");
+                salesman.setName("Lucas");
+                salesman.setSalary(2800);
+                salesman.setPercentPerSold(10);
+                salesman.setSoldAmount(1000);
+
+                System.out.println(salesman.getCode());
+                System.out.println(salesman.getName());
+                System.out.println(salesman.getSalary());
+                System.out.println(salesman.getPercentPerSold());
+                System.out.println(salesman.getSoldAmount());
+            }
+        }
+        System.out.println(employee.getfullSalary(500));
+        System.out.println(employee.getfullSalary());
+        System.out.println("============");
+
+    }
+}
---
+++ course_dio/santander_backend_2025/programação_orientada_a_objetos/02_Herança_e_polimorfismo/src/Salesman.java
@@ -0,0 +1,47 @@
+public non-sealed class Salesman extends Employee{
+
+    private double percentPerSold;
+
+    private double soldAmount;
+
+    @Override
+    public String getCode(){
+        return "SL" + this.code;
+    }
+
+    @Override
+    public double getfullSalary() {
+        return this.salary + ((soldAmount * percentPerSold) / 100);
+    }
+
+    public Salesman(String code,
+                    String name,
+                    String address,
+                    int age,
+                    double salary,
+                    double percentPerSold,
+                    double soldAmount) {
+        super(code, name, address, age, salary);
+        this.percentPerSold = percentPerSold;
+        this.soldAmount = soldAmount;
+    }
+
+    public Salesman() {
+    }
+
+    public double getPercentPerSold() {
+        return percentPerSold;
+    }
+
+    public void setPercentPerSold(double percentPerSold) {
+        this.percentPerSold = percentPerSold;
+    }
+
+    public double getSoldAmount() {
+        return soldAmount;
+    }
+
+    public void setSoldAmount(double soldAmount) {
+        this.soldAmount = soldAmount;
+    }
+}
