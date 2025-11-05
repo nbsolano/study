4a2c070a1a761fc1b2974324e2099ec1f701a64f
nbsolano <nathanbelmonte.ns@gmail.com>
2025-11-05 06:54:45 -0300

    feat: add lesson "Aula05"

---
+++ maratona_java/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+        int idade = 20;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        // !
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcoólica");
+        }else{
+            System.out.println("Não autorizado a comprar bebida alcoólica");
+        }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcoólica");
+        }
+        boolean c = true;
+
+        if (c = false){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
