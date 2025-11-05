dbeace1798b93d4f745a7cf05eda44704dab76af
nbsolano <nathanbelmonte.ns@gmail.com>
2025-11-05 06:33:06 -0300

    feat: add lesson "Aula04"

--- maratona_java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona_java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -28,7 +28,34 @@ public class Aula04Operadores {
         float salario = 3500F;
         boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
         boolean isDentroDaLeiMenorQueTrinta = idade <= 30 && salario >= 3381;
+
         System.out.println(isDentroDaLeiMenorQueTrinta);
         System.out.println(isDentroDaLeiMaiorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println(isPlaystationCincoCompravel);
+
+        // =, +=, -=, *=, /=, *=
+        double bonus = 1800;
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1; //contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(contador);
+
     }
 }
