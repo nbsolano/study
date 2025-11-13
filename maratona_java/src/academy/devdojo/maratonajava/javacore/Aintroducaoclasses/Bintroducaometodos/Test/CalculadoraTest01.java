2a0613df74a9e743f12325964402360b9e6e39e7
nbsolano <nathanbelmonte.ns@gmail.com>
2025-11-13 16:55:19 -0300

    feat: add Class CalculadoraTest01

---
+++ maratona_java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/Bintroducaometodos/Test/CalculadoraTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.Bintroducaometodos.Test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+        calculadora.subtraiDoisNumeros();
+    }
+}
