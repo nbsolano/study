46a9a6938c47cbea9b9389fc66176210d17ccb28
nbsolano <nathanbelmonte.ns@gmail.com>
2025-11-14 07:00:14 -0300

    feat: add Class EstudanteTest

---
+++ maratona_java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Nathan";
+        estudante.idade = 22;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
